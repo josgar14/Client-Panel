694cf7c92726b7e66f607c741cc3409726f61740
Josue <josgar_14@hotmail.com>
2019-08-09 01:07:24 -0400

    Client-Panel-App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyAaMLeVpKPSHGIE6r31XU5pB86eTRKhvD8',
+		authDomain: 'clientpanelprod-b3fa4.firebaseapp.com',
+		databaseURL: 'https://clientpanelprod-b3fa4.firebaseio.com',
+		projectId: 'clientpanelprod-b3fa4',
+		storageBucket: 'clientpanelprod-b3fa4.appspot.com',
+		messagingSenderId: '1071857626982',
+		appId: '1:1071857626982:web:e0603656995a13d1'
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyAaMLeVpKPSHGIE6r31XU5pB86eTRKhvD8',
+		authDomain: 'clientpanelprod-b3fa4.firebaseapp.com',
+		databaseURL: 'https://clientpanelprod-b3fa4.firebaseio.com',
+		projectId: 'clientpanelprod-b3fa4',
+		storageBucket: 'clientpanelprod-b3fa4.appspot.com',
+		messagingSenderId: '1071857626982',
+		appId: '1:1071857626982:web:e0603656995a13d1'
+	}
 };
 
 /*
